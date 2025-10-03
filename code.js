d3bca1649a99c5eed516748d6d20decabe2d4c6c
kayu475 <ada200475@gmail.com>
2025-10-03 18:52:12 +0800

    Week 2 final

---
+++ code.js
@@ -0,0 +1,37 @@
+var title=document.querySelector("h1");
+title.innerHTML="This is another title from code.js";
+
+var button= document.querySelector(".intro");
+button.addEventListener("click", function1);
+
+function function1(){
+    alert("Let me tell you more about me!");
+}
+
+var button= document.querySelector("#project");
+button.addEventListener("click", function2);
+
+function function2(){
+    alert("plenty");
+}
+
+var button= document.querySelector("#cv");
+button.addEventListener("click", function3);
+
+function function3(){
+    alert("Student of creative media");
+}
+
+var mynode= document.createElement("div");
+//change basic attributes
+mynode.id="my_works";
+mynode.innerHTML="The work is an exhibition";
+mynode.style.color="blue";
+
+//add event listener
+mynode.addEventListener("click", welcomeToWork1);
+document.querySelector("#my_works").appendChild(mynode);
+
+function welcomeToWork1(){
+    mynode.innerHTML="Thank you for your interest!"
+}
